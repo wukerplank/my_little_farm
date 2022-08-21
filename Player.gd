508ed33d4a22d5f87b64f8f462d46bbe5aaccc61
Christoph Edthofer <wukerplank@edthofer.at>
2022-08-21 11:48:32 +0200

    Lesson 1: Top down camera with a moving capsule that looks at the mouse pointer

---
+++ Player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+var speed = 5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Vector3()
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.z -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.z += 1
+
+	velocity = velocity.normalized() * speed
+
+	move_and_slide()
---
+++ main.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+
+var ray_origin = Vector3()
+var ray_target = Vector3()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var mouse_position = get_viewport().get_mouse_position()
+#	print("Mouse Position: ", mouse_position)
+
+	ray_origin = $Camera.project_ray_origin(mouse_position)
+#	print("Ray Origin: ", ray_origin)
+
+	ray_target = ray_origin + $Camera.project_ray_normal(mouse_position) * 2000
+	
+	var space_state = get_world_3d().direct_space_state
+	
+	var params = PhysicsRayQueryParameters3D.new()
+	params.from = ray_origin
+	params.to = ray_target
+	params.exclude = []
+	var intersection = space_state.intersect_ray(params)
+	
+	if not intersection.is_empty():
+		var pos = intersection.position
+		$Player.look_at(pos, Vector3.UP)
